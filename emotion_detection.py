8f14d9d2ad80bba075bccd9d32373fa31f0869ad
rodericusifo <rodericus1999@gmail.com>
2024-11-25 09:33:38 -0500

    Task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -11,4 +12,12 @@ def emotion_detector(text_to_analyze):
         } 
     }
     response = requests.post(url, headers=request_headers, json=request_body)
-    return response.text
+    parsed_response = json.loads(response.text)
+    emotions = parsed_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion_value = max(emotions.values())
+    dominant_emotion = next(
+        (key for key, value in emotions.items() if value == dominant_emotion_value),
+        None
+    )
+    emotions["dominant_emotion"] = dominant_emotion
+    return emotions
